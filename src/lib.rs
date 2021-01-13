c9004d138d847207998036aada72277d8b44914a
SK <dev.umisk@gmail.com>
2021-01-14 06:17:35 +0900

    setup subcommand

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,14 @@
-use dotenv::dotenv;
 use reqwest::{blocking::multipart, blocking::Client, blocking::Response, Result};
 use std::env;
+use std::io::prelude::*;
 
 pub fn upload(filepath: &str) -> Result<Response> {
-    dotenv().ok();
+    let home_path = dirs::home_dir()
+        .and_then(|a| Some(a.join(".streamable")))
+        .unwrap();
+
+    dotenv::from_path(home_path.as_path()).unwrap();
+
     let username = env::var("STREAMABLE_USERNAME").unwrap();
     let password = env::var("STREAMABLE_PASSWORD").unwrap();
 
@@ -24,6 +29,35 @@ pub fn upload(filepath: &str) -> Result<Response> {
     // dbg!(&resp);
 
     Ok(resp)
+}
+
+pub fn get_username_password() -> (String, String) {
+    println!("Please input Your username and password");
+    let username = rpassword::read_password_from_tty(Some("Username: ")).unwrap();
+    let password = rpassword::read_password_from_tty(Some("Password: ")).unwrap();
+    (username, password)
+}
+
+pub fn setup() -> Result<()> {
+    let env = get_username_password();
+
+    let text = format!(
+        "STREAMABLE_USERNAME={}\nSTREAMABLE_PASSWORD={}",
+        env.0, env.1
+    );
+
+    let home_path = dirs::home_dir().unwrap();
+    let config_path = home_path.join(".streamable");
+    std::fs::OpenOptions::new()
+        .write(true)
+        .create(true)
+        .open(config_path)
+        .expect("can not create config file")
+        .write_all(text.as_bytes())
+        .unwrap();
+
+    println!("saved your login params in {}", home_path.to_str().unwrap());
+    Ok(())
 }
 
 #[cfg(test)]
--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,43 @@
 use clap::{App, Arg};
 use reqwest::StatusCode;
 
-use uploader::upload;
+use uploader::{setup, upload};
 
 fn main() {
     let matches = App::new("streamable-upload")
         .version("1.0")
         .about("upload your video to streamable")
         .author("shiomiya")
-        .arg(
-            Arg::new("INPUT")
+        .subcommand(
+            App::new("upload")
                 .about("input path to your video")
-                .takes_value(true)
-                .required(true),
+                .arg(
+                    Arg::new("PATH")
+                        .about("input video path")
+                        .value_name("PATH")
+                        .takes_value(true)
+                )
         )
+        .subcommand(App::new("setup").about("set your streamable.com username and password"))
         .get_matches();
 
-    if let Some(path) = matches.value_of("INPUT") {
+    if let Some(ref matches) = matches.subcommand_matches("upload") {
+        let path = matches.value_of("PATH").unwrap();
         let response = upload(path).unwrap();
         match response.status() {
             StatusCode::OK => println!("Upload sccess!"),
-            s => println!("Received response status: {:?}", s)
+            s => println!("Received response status: {:?}", s),
+        }
+    }
+
+    if let Some(ref _matches) = matches.subcommand_matches("setup") {
+        let result = setup();
+        match result {
+            Ok(v) => {
+                println!("{:?}", v);
+                std::process::exit(0)
+            }
+            Err(e) => panic!("Error: {}", e),
         }
     }
 }
